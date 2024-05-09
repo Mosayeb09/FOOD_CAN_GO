4e7212d47d078e5749b6c48c10bb1d2c1553848c
Mosayeb09 <mosayebhossain09@gmail.com>
2024-05-10 01:07:48 +0600

    create nav and header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./Component/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 
 
 const App = () => {
   return (
     <div className="app" >
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/cart" element={<Cart></Cart>}></Route>
+        <Route path="/placeOrder" element={<PlaceOrder></PlaceOrder>}></Route>
+      </Routes>
     </div>
   );
 };
