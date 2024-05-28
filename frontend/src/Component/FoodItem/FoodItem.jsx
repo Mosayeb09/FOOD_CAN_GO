38b4f996963fe29842a1505d247d882798f513ee
Mosayeb09 <mosayebhossain09@gmail.com>
2024-05-28 17:28:32 +0600

    adding card count function

--- frontend/src/Component/FoodItem/FoodItem.jsx
+++ frontend/src/Component/FoodItem/FoodItem.jsx
@@ -1,11 +1,18 @@
 
+import { useState } from 'react';
 import { assets } from '../../assets/assets';
 import './FoodItem.css'
 const FoodItem = ({id,name,price,description,image}) => {
+    const [itemCount,setItemCount]=useState(0)
     return (
         <div className='food-item'>
             <div className="food-item-container">
                 <img className='food-item-img' src={image} alt="" />
+                {!itemCount
+                   ?<img className='add'onClick={()=>setItemCount(prev =>prev+1)} src={assets.add_icon_white} alt="" />
+                   :<div className='food-item-counter'></div>
+
+                }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
