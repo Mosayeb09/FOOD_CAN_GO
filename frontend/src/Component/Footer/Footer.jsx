f0ea39fc79c20ed961f9bdb07e9c81b0f460aa3b
Mosayeb09 <mosayebhossain09@gmail.com>
2024-05-29 18:00:31 +0600

    adding footer section

--- frontend/src/Component/Footer/Footer.jsx
+++ frontend/src/Component/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
                 <div className="footer-content-left">
                     <img src={assets.logo} alt="" />
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi non ullam fuga, assumenda asperiores minima nobis quam voluptatem, facilis tenetur veniam sequi eaque maiores perferendis esse nemo rerum ducimus dignissimos.</p>
-                    <div className="footer-social-item">
+                    <div className="footer-social-icons">
                         <img src={assets.facebook_icon} alt="" />
                         <img src={assets.twitter_icon} alt="" />
                         <img src={assets.linkedin_icon} alt="" />
@@ -17,10 +17,13 @@ const Footer = () => {
                 </div>
                 <div className="footer-content-center">
                     <h2>COMPANY</h2>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Delivery</li>
-                    <li>Privacy policy</li>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+
 
                 </div>
                 <div className="footer-content-right">
@@ -34,7 +37,7 @@ const Footer = () => {
             </div>
             <hr />
             <p className='footer-copyRight'>Copyright 2024 © Tomato.com - All rights reserved.</p>
-            
+
         </div>
     );
 };
