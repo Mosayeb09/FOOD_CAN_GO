0ad8865c279171bc47b977bb37521e51510db465
Mosayeb09 <mosayebhossain09@gmail.com>
2024-05-29 18:15:54 +0600

    adding appDownload section

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import ExploreMenu from "../../Component/ExploreMenu/ExploreMenu";
 import Header from "../../Component/Header/Header";
 import "./Home.css"
 import FoodDisplay from "../../Component/FoodDisplay/FoodDisplay";
+import AppDownload from "../../Component/AppDownload/AppDownload";
 
 const Home = () => {
     const [category,setCategory]= useState('All');
@@ -12,6 +13,7 @@ const Home = () => {
             <Header></Header>
             <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
             <FoodDisplay category={category}></FoodDisplay>
+            <AppDownload></AppDownload>
         </div>
     );
 };
