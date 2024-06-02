2afc282c881943d6b6534ffe78df167e21f25d59
Mosayeb09 <mosayebhossain09@gmail.com>
2024-06-02 18:01:13 +0600

    creating login form

--- frontend/src/Component/LoginPopup/LoginPopup.jsx
+++ frontend/src/Component/LoginPopup/LoginPopup.jsx
@@ -11,6 +11,25 @@ const LoginPopup = ({setshowForm}) => {
                     <h2>{currentstate}</h2>
                     <img onClick={()=>setshowForm(false)} src={assets.cross_icon} alt="" />
                 </div>
+                <div className="login-popup-input">
+                    {
+                        currentstate==='Login'?<></>: <input type="text"placeholder='Your Name'required
+                        />
+                    }
+                   
+                     <input type="email" name="" id="" placeholder='Your email'required />
+                     <input type="password" placeholder='password' required/>
+                </div>
+                <button>{currentstate==='sign Up'?'Create account':'Login'}</button>
+                <div className="login-popup-condition">
+                    <input type="checkbox" name="" id=""required />
+                    <p>By continuing,i agree to the terms of use & privacy policy.</p>
+                </div>
+                {
+                    currentstate==='Login'?<p>create a new account? <span onClick={()=>setcurrentstate('Sign Up')}>click here</span></p>:<p>Already have an account? <span onClick={()=>setcurrentstate('Login')}>Login here</span></p>
+                }
+                
+                
             </form>
         </div>
     );
