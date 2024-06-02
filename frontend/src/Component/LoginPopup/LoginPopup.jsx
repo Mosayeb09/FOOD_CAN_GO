15233474327f1202dc6a362e702ad05a9e4b8544
Mosayeb09 <mosayebhossain09@gmail.com>
2024-06-02 18:21:53 +0600

    compleating login and sign up form

--- frontend/src/Component/LoginPopup/LoginPopup.jsx
+++ frontend/src/Component/LoginPopup/LoginPopup.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/assets';
 const LoginPopup = ({setshowForm}) => {
     const [currentstate,setcurrentstate]=useState('Sign Up')
     return (
-        <div className='loginpopup'>
+        <div className='login-popup'>
             <form className='login-popup-container'>
                 <div className="login-popup-title">
                     <h2>{currentstate}</h2>
@@ -20,7 +20,7 @@ const LoginPopup = ({setshowForm}) => {
                      <input type="email" name="" id="" placeholder='Your email'required />
                      <input type="password" placeholder='password' required/>
                 </div>
-                <button>{currentstate==='sign Up'?'Create account':'Login'}</button>
+                <button>{currentstate==='Sign Up'?"Create account":'Login'}</button>
                 <div className="login-popup-condition">
                     <input type="checkbox" name="" id=""required />
                     <p>By continuing,i agree to the terms of use & privacy policy.</p>
